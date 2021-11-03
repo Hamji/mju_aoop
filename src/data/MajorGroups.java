65d0e30f88ef2691313c02d7e216921b9a73cfa7
yunanp22 <yunanp22@gmail.com>
2021-11-03 09:58:31 +0900

    Religion.java, MajorGroups.java modified

--- src/data/MajorGroups.java
+++ src/data/MajorGroups.java
@@ -1,29 +1,18 @@
-package data;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class MajorGroups {
 	private Map<String, Double> MGMap;
 	
-	public MajorGroups(String grp, Double db) {
-		MGMap = new HashMap<String, Double>();
-		if(db == null) {
-			this.MGMap.put(grp, 0.0);
-		}
-		this.MGMap.put(grp, db);
+	public MajorGroups() {
+		this.MGMap = new HashMap<String, Double>();
 	}
 	
-	public MajorGroups() {
-		// TODO Auto-generated constructor stub
+	public void setMGData(String rg, Double db) {
+		this.MGMap.put(rg, db);
 	}
-
+	
 	public Map<String, Double> getMGData() {
 		return this.MGMap;
-	}
-
-	public void setMGData(String mg_name, Double mg_rate) {
-		// TODO Auto-generated method stub
-		
 	}
 }
--- src/data/Religion.java
+++ src/data/Religion.java
@@ -1,29 +1,18 @@
-package data;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class Religion {
 	private Map<String, Double> RMap;
 	
-	public Religion(String rg, Double db) {
-		RMap = new HashMap<String, Double>();
-		if(db == null) {
-			this.RMap.put(rg, 0.0);
-		}
+	public Religion() {
+		this.RMap = new HashMap<String, Double>();
+	}
+	
+	public void setRData(String rg, Double db) {
 		this.RMap.put(rg, db);
 	}
 	
-	public Religion() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public Map<String, Double> getRData() {
 		return this.RMap;
-	}
-
-	public void setRData(String rg_name, Double rg_rate) {
-		// TODO Auto-generated method stub
-		
 	}
 }
