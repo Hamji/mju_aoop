c91ba94473aeaec2511f593c9150a3fdb6065ed9
BEMELON <hgdkim2@naver.com>
2021-11-05 20:59:01 +0900

    Update Filter remove Btn

--- src/gui/ApplyedListPanel.java
+++ src/gui/ApplyedListPanel.java
@@ -1,6 +1,9 @@
 package gui;
 
-import java.awt.BorderLayout;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Arrays;
 
 import javax.swing.*;
 
@@ -8,18 +11,37 @@ public class ApplyedListPanel extends BasicPanel {
 	private DefaultListModel items;
 	private JList list;
 	private JLabel label;
-	
+
+	private class TopPanel extends BasicPanel {
+		public TopPanel() {
+			setLayout(new GridLayout(1, 2));
+			JButton button = new JButton("삭제");
+			button.addActionListener(e -> removeFilter());
+
+			add(label);
+			add(button);
+		}
+	}
+
 	public ApplyedListPanel() {
 		this.setLayout(new BorderLayout(0, 0));
 		items = new DefaultListModel();
 		list = new JList(items);
 		label = new JLabel("적용된 필터");
 		
-		this.add(label, BorderLayout.NORTH);
+		this.add(new TopPanel(), BorderLayout.NORTH);
 		this.add(list, BorderLayout.CENTER);
 	}
 
 	public void addFilter(String filter) {
 		items.add(list.getModel().getSize(), filter);
+	}
+
+	public void removeFilter() {
+		int[] selectedIndices = list.getSelectedIndices();
+
+		for(int i = selectedIndices.length - 1; i >= 0; i--) {
+			items.remove(selectedIndices[i]);
+		}
 	}
 }
