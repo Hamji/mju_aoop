fc19efa10240719077415ff8f4be870df84da877
Hamji <rlarjsgml919@naver.com>
2021-11-22 18:51:57 +0900

    Country list update

--- src/gui/CountryListPanel.java
+++ src/gui/CountryListPanel.java
@@ -1,8 +1,19 @@
 package gui;
 
+import java.awt.BorderLayout;
+
 import javax.swing.DefaultListModel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 
 public class CountryListPanel extends BasicPanel {
-	private DefaultListModel model;
+	private String[] dummyList = {"test", "test", "test", "test", "test", "test",
+			 "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test"
+			 , "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test"};
+	public CountryListPanel() {
+		this.setLayout(new BorderLayout());
+		JList temp = new JList(this.dummyList);
+		this.add(new JScrollPane(temp), BorderLayout.CENTER);
+	}
 	
 }
