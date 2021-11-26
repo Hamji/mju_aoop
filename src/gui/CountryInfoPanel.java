bcac7e68317a3169b1efa06c687dd81a7bdc5b6b
Hamji <rlarjsgml919@naver.com>
2021-11-27 01:32:04 +0900

    graph add

--- src/gui/CountryInfoPanel.java
+++ src/gui/CountryInfoPanel.java
@@ -1,8 +1,10 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Desktop;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -14,6 +16,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.nio.ByteOrder;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -55,11 +58,44 @@ public class CountryInfoPanel extends BasicPanel {
    private TextArea inform;
    private static final String url = "https://www.tripadvisor.co.kr/Search?q=";
    private JButton urlButton;
+   private ChartPanel chartPanel;
    
    private JPanel infoPanel;
    
    private String dummyName = "Test Name";
-   private String dummyInfo = "Test Information";
+   private String infoString;
+   
+   private Map<String, Double> rMap;
+   
+   Color[] color = {Color.RED, Color.BLUE, Color.CYAN, Color.ORANGE,
+		   Color.MAGENTA, Color.GREEN, Color.YELLOW };
+   
+   class ChartPanel extends JPanel{
+	  public void paintComponent(Graphics g) {
+		  super.paintComponent(g);
+		  
+		  int startAngle = 0;
+		  
+		  
+		  if (rMap.isEmpty()) {
+			  System.out.println("null");
+			  return;
+		  }
+		  String[] keyArr = rMap.keySet().toArray(new String[0]);
+		  
+		  for (int i = 0; i < keyArr.length; i++) {
+			  g.setColor(color[i]);
+			  int temp = (int) Math.round(rMap.get(keyArr[i]));
+			  g.drawString( keyArr[i] + " : " + Integer.toString(temp) + "%", 10 , 20 + i * 20);
+			  //System.out.println(keyArr[i]);
+		  }
+		  for (int i = 0; i < keyArr.length; i++) {
+			  g.setColor(color[i]);
+			  g.fillArc(150, 50, 100, 100, startAngle, (int)(rMap.get(keyArr[i]) * 36 / 10.0));
+			  startAngle += (int)(rMap.get(keyArr[i]) * 36 / 10.0);
+		  }
+	  }
+   }
    
    public CountryInfoPanel() {
       this.name = new JLabel("Empty Name");
@@ -96,7 +132,6 @@ public class CountryInfoPanel extends BasicPanel {
       this.infoPanel.setLayout(new GridLayout(2,1));
       
       this.infoPanel.add(this.inform);
-      this.infoPanel.add(new JButton("hello"));
       
       this.add(this.name, BorderLayout.NORTH);
       this.add(this.infoPanel, BorderLayout.CENTER);
@@ -104,15 +139,40 @@ public class CountryInfoPanel extends BasicPanel {
    }
 
    public void drawInfo(String country) {
-      //System.out.println(country);
+      //System.out.println("이 나라는 " + country);
 	  Data d = Data.getInstance();
-	  CGIDTO temp;
+	  CGIDTO temp = d.getCountryData(country);
+	  
+	  
+	  // name update
 	  this.name.setText(country);
+	  
+	  // url button update
 	  this.remove(urlButton);
 	  this.urlButton = null;
 	  this.urlButton = new JButton("더 많은 정보");
 	  URIListener ul = new URIListener(this.name.getText());
 	  this.urlButton.addActionListener(ul);
 	  this.add(urlButton, BorderLayout.SOUTH);
+   
+	  // infomation update
+	  this.infoString = new String("");
+	  this.infoString += "위치 : " + temp.getLocation() +'\n';
+	  this.infoString += "수도 : " + temp.getCapital() + '\n';
+	  this.infoString += "주요 도시 : " + temp.getMajorCity() + '\n';
+	  this.infoString += "기후 : " + temp.getClimate() + '\n';
+	  this.infoString += "국가 번호 : " + temp.getCountryCode() + '\n';
+	  this.infoString += "사용 언어 : " + temp.getLanguage() + '\n';
+	  this.infoString += "종교 : " + temp.getReligion().toString();
+	  this.inform.setText(infoString);
+	  
+	  // graph
+	  this.rMap = temp.getReligion().getRData();
+	  if (chartPanel != null)
+		  this.infoPanel.remove(chartPanel);
+	  this.chartPanel = new ChartPanel();
+	  this.infoPanel.add(this.chartPanel);
+	  
+	  this.repaint();
    }
 }
